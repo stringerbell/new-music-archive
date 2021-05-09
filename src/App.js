b756c1328377ad198988c9d0d8128135221a92ad
Dann Stockton <dannstockton@gmail.com>
2021-05-08 20:24:57 -0600

    Add css polish

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {useEffect, useState} from 'react';
 function Artwork({artwork, width = 100, height = 100}) {
     return (
         <>
-            <img src={artwork.url.replaceAll("{w}", width).replaceAll("{h}", height)} alt="TODO"/>
+            <img className={'album-art'} src={artwork.url.replaceAll("{w}", width).replaceAll("{h}", height)} alt="TODO"/>
         </>
     )
 }
@@ -13,8 +13,17 @@ function Album({album}) {
     const {attributes} = album
     return <div>
         <a href={attributes.url}><Artwork width={200} height={200} artwork={attributes.artwork}/></a>
-        <div className={"artist-name"}>{album.attributes.artistName}</div>
-        <div className={"album-name"}>{album.attributes.name}</div>
+        <div className={"artist-name"}>
+            <p>
+                {album.attributes.artistName}
+            </p>
+                <span className={"explicit"}>{attributes.contentRating === "explicit" && "🅴"}</span>
+        </div>
+        <div>
+            <p className={"album-name"}>
+                {album.attributes.name}
+            </p>
+        </div>
     </div>
 }
 

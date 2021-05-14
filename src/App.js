4897c32f7ac507af79de001949be14267dfe7b04
Dann Stockton <dannstockton@gmail.com>
2021-05-14 06:45:24 -0600

    Remove Datepicker

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
 import NewAlbumList from "./AlbumList";
 
-
-function Datepicker() {
-    // const files = ['2021-05-07']
-    return null
-}
-
 function App() {
     return (
         <div className="App">
-            <Datepicker />
             <NewAlbumList/>
         </div>
     );

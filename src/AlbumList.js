7d4f32b746cc9c40d4cfe18a18f6be85a147a962
Dann Stockton <dannstockton@gmail.com>
2021-05-14 06:45:15 -0600

    Add new dates

--- src/AlbumList.js
+++ src/AlbumList.js
@@ -3,16 +3,29 @@ import Album from "./Album";
 
 export default function NewAlbumList() {
     const [albums, setAlbums] = useState([])
+    const dates = [
+        '2021-05-14',
+        '2021-05-07',
+    ];
+    const [date, setDate] = useState(dates[0])
     useEffect(() => {
-        get()
-    }, [])
-    const get = async () => {
-        const res = await fetch("/new-music-archive/archive/2021-05-07.json");
+        get(date)
+    }, [date])
+    const get = async (date) => {
+        const res = await fetch(`/new-music-archive/archive/${date}.json`);
         const {data} = await res.json();
         setAlbums(data)
     }
     const as = albums.map((album) => {
         return <Album album={album} key={album.id}/>
     })
-    return <div className={"album-list"}>{as}</div>
+    return (
+        <>
+            <label htmlFor="dates">Choose a date:</label>
+            <select name="dates" onChange={(e) => setDate(e.target.value)}>
+                {dates.map(d => <option key={d} value={d}>{d}</option>)}
+            </select>
+            <div className={"album-list"}>{as}</div>
+        </>
+    )
 }

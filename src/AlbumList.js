90c58be1d2a642c9bd453d7172c342e836acd1c8
Dann Stockton <dannstockton@gmail.com>
2021-05-16 14:13:04 -0600

    Make dates dumber, and more correct

--- src/AlbumList.js
+++ src/AlbumList.js
@@ -5,14 +5,14 @@ import {format} from 'date-fns'
 export default function NewAlbumList() {
     const [albums, setAlbums] = useState([])
     const dates = [
-        '2021-05-14',
-        '2021-05-07',
+        {date: '2021-05-14', name: "Friday, May 14th 2021"},
+        {date: '2021-05-07', name: "Friday, May 7th 2021"},
     ];
     const [date, setDate] = useState(dates[0])
     useEffect(() => {
         get(date)
     }, [date])
-    const get = async (date) => {
+    const get = async ({date}) => {
         const res = await fetch(`/new-music-archive/archive/${date}.json`);
         const {data} = await res.json();
         setAlbums(data)
@@ -23,8 +23,10 @@ export default function NewAlbumList() {
     return (
         <>
             <label htmlFor="dates">Choose a date:</label>
-            <select className={'date-selection'} name="dates" onChange={(e) => setDate(e.target.value)}>
-                {dates.map(d => <option key={d} value={d}>{format(new Date(d), 'eeee, MMM do, yyyy')}</option>)}
+            <select className={'date-selection'} name="dates" onChange={(e) => setDate({date: e.target.value})}>
+                {dates.map(({name, date}) => {
+                    return (<option key={date} value={date}>{name}</option>)
+                })}
             </select>
             <div className={"album-list"}>{as}</div>
         </>

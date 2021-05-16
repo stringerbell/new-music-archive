7a0f396a138ea78e85c922ff2ba1bb8deb91684d
Dann Stockton <dannstockton@gmail.com>
2021-05-16 14:31:45 -0600

    Add cache state

--- src/AlbumList.js
+++ src/AlbumList.js
@@ -3,22 +3,25 @@ import Album from "./Album";
 
 export default function NewAlbumList() {
     const [albums, setAlbums] = useState([])
+    let [cache, addCache] = useState({})
     const dates = [
         {date: '2021-05-14', name: "Friday, May 14th 2021"},
         {date: '2021-05-07', name: "Friday, May 7th 2021"},
     ];
     const [date, setDate] = useState(dates[0])
     useEffect(() => {
-        get(date)
-    }, [date])
-    const get = async ({date}) => {
-        const res = await fetch(`/new-music-archive/archive/${date}.json`);
-        const {data} = await res.json();
-        setAlbums(data)
-    }
-    const as = albums.map((album) => {
-        return <Album album={album} key={album.id}/>
-    })
+        (async ({date}) => {
+            if (cache[date]) {
+                setAlbums(cache[date]);
+                return
+            }
+            const res = await fetch(`/new-music-archive/archive/${date}.json`);
+            const {data} = await res.json();
+            cache[date] = data
+            addCache(cache)
+            setAlbums(data)
+        })(date)
+    }, [cache, date])
     return (
         <>
             <label htmlFor="dates">Choose a date:</label>
@@ -27,7 +30,9 @@ export default function NewAlbumList() {
                     return (<option key={date} value={date}>{name}</option>)
                 })}
             </select>
-            <div className={"album-list"}>{as}</div>
+            <div className={"album-list"}>{albums.map((album) => {
+                return <Album album={album} key={album.id}/>
+            })}</div>
         </>
     )
 }

73376487242e968b76a52ca685ba100889f7ef2e
Dann Stockton <dannstockton@gmail.com>
2021-08-14 07:16:02 -0600

    Add links to main page with dates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import NewAlbumList from "./AlbumList";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import Dates from "./Dates";
 
 function App() {
     return (
+        <Router>
         <div className="App">
-            <NewAlbumList/>
+            <Switch>
+            <Route path={'/:date'}>
+                <NewAlbumList />}/>
+            </Route>
+            </Switch>
         </div>
+        </Router>
     );
 }
 

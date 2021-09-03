0eb781a03fb4d5fc8f877744138fbf8d8a784b23
Dann Stockton <dannstockton@gmail.com>
2021-09-03 05:41:08 -0600

    Add Sep 3

--- src/Dates.js
+++ src/Dates.js
@@ -1,6 +1,7 @@
 export default function useDates() {
     return (
         [
+            {date: '2021-09-03', name: "Friday, September 3rd 2021"},
             {date: '2021-08-27', name: "Friday, August 27th 2021"},
             {date: '2021-08-20', name: "Friday, August 20th 2021"},
             {date: '2021-08-13', name: "Friday, August 13th 2021"},

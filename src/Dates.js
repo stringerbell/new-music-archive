e58651040697d947c252a05fdf60e61e0e5c02c3
Dann Stockton <dannstockton@gmail.com>
2021-12-03 08:52:11 -0700

    Add Dec 3

--- src/Dates.js
+++ src/Dates.js
@@ -1,6 +1,7 @@
 export default function useDates() {
     return (
         [
+            {date: '2021-12-03', name: "Friday, December 3rd 2021"},
             {date: '2021-11-26', name: "Friday, November 26th 2021"},
             {date: '2021-11-19', name: "Friday, November 19th 2021"},
             {date: '2021-11-12', name: "Friday, November 12th 2021"},

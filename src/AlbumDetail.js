ca54b2f8bdd6c7c5dbbe1ad47728b10d08abe283
Dann Stockton <dannstockton@gmail.com>
2022-03-17 05:46:46 -0600

    Add meta tags

--- src/AlbumDetail.js
+++ src/AlbumDetail.js
@@ -1,6 +1,7 @@
 import Artwork from './Artwork';
 import { Link, useParams } from 'react-router-dom';
-import ListenLinks from "./ListenLinks";
+import ListenLinks from './ListenLinks';
+import { MetaTags } from 'react-meta-tags';
 
 const get = (p) => (o) => p.reduce((xs, x) => (xs && xs[x] ? xs[x] : null), o);
 
@@ -19,16 +20,49 @@ export default function AlbumDetail({ album }) {
       <Link className={'back-link'} to={`/${date}`}>
         {'＜ Back'}
       </Link>
+      <MetaTags>
+        <title>{`${attributes.artistName} - ${attributes.name}`}</title>
+        <meta name='description' content={attributes.editorialNotes.short} />
+        <meta property='og:title' content={attributes.name} />
+
+        <meta property='og:url' content={window.location.href} />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:description'
+          content={attributes.editorialNotes.short}
+        />
+        <meta
+          property='og:image'
+          content={attributes.artwork.url
+            .replaceAll('{w}', 650)
+            .replaceAll('{h}', 650)}
+        />
+
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta property='twitter:domain' content={window.location.origin} />
+        <meta property='twitter:url' content={window.location.href} />
+        <meta name='twitter:title' content={attributes.name} />
+        <meta
+          name='twitter:description'
+          content={attributes.editorialNotes.short}
+        />
+        <meta
+          name='twitter:image'
+          content={attributes.artwork.url
+            .replaceAll('{w}', 650)
+            .replaceAll('{h}', 650)}
+        />
+      </MetaTags>
       <div className={'album'}>
         <div className={'wrapper'}>
           <a href={attributes.url}>
             <Artwork
-                title={title}
-                className={'album-art'}
-                width={650}
-                height={650}
-                artwork={attributes.artwork}
-                alt={`${attributes.artistName} - ${attributes.name} album artwork`}
+              title={title}
+              className={'album-art'}
+              width={650}
+              height={650}
+              artwork={attributes.artwork}
+              alt={`${attributes.artistName} - ${attributes.name} album artwork`}
             />
           </a>
           <div className={'detail-album-details'}>
@@ -46,11 +80,11 @@ export default function AlbumDetail({ album }) {
             <div className={'album-genre'}>
               {genre} - {year}
             </div>
-            <ListenLinks attributes={attributes}/>
+            <ListenLinks attributes={attributes} />
             <div className={'notes-wrap'}>
               <p
-                  className={'detail-editors-notes'}
-                  dangerouslySetInnerHTML={setNotes(notes)}
+                className={'detail-editors-notes'}
+                dangerouslySetInnerHTML={setNotes(notes)}
               />
             </div>
           </div>
--- src/AlbumList.js
+++ src/AlbumList.js
@@ -1,51 +1,86 @@
-import {useEffect, useState} from "react";
-import Album from "./Album";
-import {useParams, useHistory} from "react-router-dom";
-import useDates from "./Dates";
+import { useEffect, useState } from 'react';
+import Album from './Album';
+import { useParams, useHistory } from 'react-router-dom';
+import useDates from './Dates';
+import { MetaTags } from 'react-meta-tags';
 
 export default function NewAlbumList() {
-    const dates = useDates();
-    let {date: dateP} = useParams();
-    let history = useHistory();
-    const [albums, setAlbums] = useState([])
-    let [cache, addCache] = useState({})
+  const dates = useDates();
+  let { date: dateP } = useParams();
+  let history = useHistory();
+  const [albums, setAlbums] = useState([]);
+  let [cache, addCache] = useState({});
 
-    let picked = 0;
-    for (let i = 0; i < dates.length; i++) {
-        if (dates[i]["date"] === dateP) {
-            picked = i
-            break
-        }
+  let picked = 0;
+  for (let i = 0; i < dates.length; i++) {
+    if (dates[i]['date'] === dateP) {
+      picked = i;
+      break;
     }
-    const [date, setDate] = useState(dates[picked])
+  }
+  const [date, setDate] = useState(dates[picked]);
 
-    useEffect(() => {
-        (async ({date}) => {
-            if (cache[date]) {
-                setAlbums(cache[date]);
-                return
-            }
-            const res = await fetch(`/new-music-archive/archive/${date}.json`);
-            const {data} = await res.json();
-            cache[date] = data
-            addCache(cache)
-            setAlbums(data)
-        })(date)
-    }, [cache, date])
-    return (
-        <>
-            <select className={'date-selection'} value={date.date} name="dates"
-                    onChange={(e) => {
-                        setDate({date: e.target.value})
-                        history.push(`/${e.target.value}`)
-                    }}>
-                {dates.map(({name, date}) => {
-                    return (<option key={date} value={date}>{name}</option>)
-                })}
-            </select>
-            <div className={"album-list"}>{albums.map((album, i) => {
-                return <Album idx={i} album={album} key={album.id}/>
-            })}</div>
-        </>
-    )
+  useEffect(() => {
+    (async ({ date }) => {
+      if (cache[date]) {
+        setAlbums(cache[date]);
+        return;
+      }
+      const res = await fetch(`/new-music-archive/archive/${date}.json`);
+      const { data } = await res.json();
+      cache[date] = data;
+      addCache(cache);
+      setAlbums(data);
+    })(date);
+  }, [cache, date]);
+  return (
+    <>
+      <MetaTags>
+        <title>{`New Music Archive - ${dateP}`}</title>
+        <meta
+          name='description'
+          content={"Apple's list of new music. Archived every week."}
+        />
+        <meta property='og:title' content={`New Music Archive - ${dateP}`} />
+
+        <meta property='og:url' content={window.location.href} />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:description'
+          content={"Apple's list of new music. Archived every week."}
+        />
+
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta property='twitter:domain' content={window.location.origin} />
+        <meta property='twitter:url' content={window.location.href} />
+        <meta name='twitter:title' content={`New Music Archive - ${dateP}`} />
+        <meta
+          name='twitter:description'
+          content={"Apple's list of new music. Archived every week."}
+        />
+      </MetaTags>
+      <select
+        className={'date-selection'}
+        value={date.date}
+        name='dates'
+        onChange={(e) => {
+          setDate({ date: e.target.value });
+          history.push(`/${e.target.value}`);
+        }}
+      >
+        {dates.map(({ name, date }) => {
+          return (
+            <option key={date} value={date}>
+              {name}
+            </option>
+          );
+        })}
+      </select>
+      <div className={'album-list'}>
+        {albums.map((album, i) => {
+          return <Album idx={i} album={album} key={album.id} />;
+        })}
+      </div>
+    </>
+  );
 }

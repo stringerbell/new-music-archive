9b0f5e8c15155a5a84601da19bb30cf23a9191c1
Dann Stockton <dannstockton@gmail.com>
2022-06-10 07:12:56 -0600

    Add June 10

--- src/Dates.js
+++ src/Dates.js
@@ -1,6 +1,7 @@
 export default function useDates() {
     return (
         [
+            {date: '2022-06-10', name: "Friday, June 10th 2022"},
             {date: '2022-06-03', name: "Friday, June 3rd 2022"},
             {date: '2022-05-27', name: "Friday, May 27th 2022"},
             {date: '2022-05-20', name: "Friday, May 20th 2022"},

e789e13e0fd4231baf4b5c03df9b540e681b50c1
Dann Stockton <dannstockton@gmail.com>
2023-06-30 08:31:37 -0600

    Add Jun 30

--- src/Dates.js
+++ src/Dates.js
@@ -1,6 +1,7 @@
 export default function useDates() {
     return (
         [
+            {date: '2023-06-30', name: "Friday, June 30th 2023"},
             {date: '2023-06-23', name: "Friday, June 23rd 2023"},
             {date: '2023-06-16', name: "Friday, June 16th 2023"},
             {date: '2023-06-09', name: "Friday, June 9th 2023"},

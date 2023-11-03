428fe0073491552081b441b7579f46bcaf897ac4
Dann Stockton <dannstockton@gmail.com>
2023-11-03 07:37:21 -0600

    Add Nov 3

--- src/Dates.js
+++ src/Dates.js
@@ -1,6 +1,7 @@
 export default function useDates() {
     return (
         [
+            {date: '2023-11-03', name: "Friday, November 3rd 2023"},
             {date: '2023-10-27', name: "Friday, October 27th 2023"},
             {date: '2023-10-20', name: "Friday, October 20th 2023"},
             {date: '2023-10-13', name: "Friday, October 13th 2023"},

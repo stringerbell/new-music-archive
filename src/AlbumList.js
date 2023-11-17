2d64b522264ac47e0c3c64ce8c8998f00af291f2
Dann Stockton <dannstockton@gmail.com>
2023-11-17 10:00:00 -0700

    Functions so nice, we're doin em twice

--- src/AlbumList.js
+++ src/AlbumList.js
@@ -27,7 +27,13 @@ export default function NewAlbumList() {
         return;
       }
       const res = await fetch(`/new-music-archive/archive/${date}.json`);
-      const { data } = await res.json();
+      let { data, resources } = await res.json();
+      if (data[0].type === 'rooms') {
+        // TODO consolidate AlbumList.js/useAlbum.js
+        // Kinda hacky, but this is b/c apple changed their format. There's probably a lot more data we could be using.
+        // We should also maybe backfill the old formats with the new one, so we don't have to juggle
+        data = Object.values(resources['albums'])
+      }
       cache[date] = data;
       addCache(cache);
       setAlbums(data);

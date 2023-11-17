4c4fb31d2b122f8e0fc71399db80766d3e006857
Dann Stockton <dannstockton@gmail.com>
2023-11-17 09:59:36 -0700

    Make a TODO for broken genres

--- src/useGenre.js
+++ src/useGenre.js
@@ -1,5 +1,7 @@
 
 export default function useGenre(album) {
+  // TODO -- this is currently broken as of nov 17. The genre now comes from the top-level artist by id block, so we'll
+  //  need to slurp that in here too
   // new format
   const genre = album?.relationships?.artists?.data[0]?.attributes?.genreNames
   if (genre) {

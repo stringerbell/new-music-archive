439c43d11b31f474aad0ace77385ee6907fa4d47
Dann Stockton <dannstockton@gmail.com>
2024-09-27 08:47:41 -0600

    Add Sep 27

--- src/Dates.js
+++ src/Dates.js
@@ -1,6 +1,7 @@
 export default function useDates() {
     return (
         [
+            {date: '2024-09-27', name: "Friday, September 27th 2024"},
             {date: '2024-09-20', name: "Friday, September 20th 2024"},
             {date: '2024-09-13', name: "Friday, September 13th 2024"},
             {date: '2024-09-06', name: "Friday, September 6th 2024"},

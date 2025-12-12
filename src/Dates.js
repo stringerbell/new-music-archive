5506432aab3c38b6ed9c2f46dab704b49e859780
Dann Stockton <dannstockton@gmail.com>
2025-12-12 08:26:44 -0700

    Add Dec 12

--- src/Dates.js
+++ src/Dates.js
@@ -1,6 +1,7 @@
 export default function useDates() {
     return (
         [
+            {date: '2025-12-12', name: "Friday, December 12th 2025"},
             {date: '2025-12-05', name: "Friday, December 5th 2025"},
             {date: '2025-11-28', name: "Friday, November 28th 2025"},
             {date: '2025-11-21', name: "Friday, November 21st 2025"},
